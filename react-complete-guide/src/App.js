7ae3ea8c4aa3e2eef18e30b0318b222546825bce
github username <github email address>
2020-05-04 19:52:37 -0400

    import update

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import person from './Person/Person';
 
 class App extends Component {
   state = {
