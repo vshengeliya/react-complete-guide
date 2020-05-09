e451199574bbaa9e024c5e869e22af36cda4d6a8
github username <github email address>
2020-05-09 12:54:27 -0400

    removed styled components from app.js

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,22 +1,7 @@
 import React, { Component } from 'react'; 
-import styled from 'styled-components';
 import './App.css';
 
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-      background-color: ${props=> props.alt ? 'red': 'green'};
-      color: white;
-      font: inherit;
-      border: 2px solid blue;
-      padding: 8px;
-      cursor: pointer;
-
-      &:hover{
-        background-color: ${props=> props.alt ? 'salmon':'lightgreen'};
-        color: black;
-      }
-`;
 
 class App extends Component {
   state = {
@@ -111,10 +96,8 @@ deletePersonHandler = (personIndex)=>{
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <StyledButton
-         alt={this.state.showPersons}
-         onClick={this.togglePersonsHandler}>Toggle Persons
-        </StyledButton>
+        <button className='button' onClick={this.togglePersonsHandler}>Toggle Persons
+        </button>
        {persons}
       </div>
     );
