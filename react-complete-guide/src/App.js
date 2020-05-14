677315c0d0554e23e5966a692ad3e0a488657414
github username <github email address>
2020-05-14 19:01:10 -0400

    App.module.css

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'; 
-import './App.css';
+import classes from './App.module.css';
 
 import Person from './Person/Person';
 
@@ -83,20 +83,20 @@ deletePersonHandler = (personIndex)=>{
       // }
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <=2){
-      classes.push('bold')
+      assignedClasses.push(classes.bold)
     }
 
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button className='button' onClick={this.togglePersonsHandler}>Toggle Persons
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button className='classes.Button' onClick={this.togglePersonsHandler}>Toggle Persons
         </button>
        {persons}
       </div>
